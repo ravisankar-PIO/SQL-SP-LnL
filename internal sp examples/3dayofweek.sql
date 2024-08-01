a2af41692ae2f2c4b1d2d7c49194e6cc89111b0a
Ravisankar Pandian <139198015+ravisankar-PIO@users.noreply.github.com>
2024-08-01 12:58:45 +0530

    Update 3dayofweek.sql

--- internal sp examples/3dayofweek.sql
+++ internal sp examples/3dayofweek.sql
@@ -1,21 +1,23 @@
-create or replace procedure cmpsys.getdayofweek(
-  in input_date date, 
-  out output_day varchar(10)
+create or replace procedure cmpsys.RetrieveSeniorEmployee(
+  in  deptId        char(3), 
+  out employeeName  varchar(30),
+  out DOB           date
 )
 language sql
+reads sql data
+specific retolder
+not deterministic
 begin
-    set output_day = case dayofweek(input_date)
-        when 1 then 'Sunday'
-        when 2 then 'Monday'
-        when 3 then 'Tuesday'
-        when 4 then 'Wednesday'
-        when 5 then 'Thursday'
-        when 6 then 'Friday'
-        when 7 then 'Saturday'
-        else 'invalid'
-    end;
+    select  firstnme||' '||lastname, birthdate 
+    into    employeeName, DOB
+    from    cmpsys.employee
+    where   workdept = deptID
+    order by birthdate 
+    fetch first row only
 end;
 
--- create variable cmpsys.mychar char(10);
--- call cmpsys.getdayofweek('2024-12-08',cmpsys.mychar );
--- values(cmpsys.mychar);
+-- create variable cmpsys.mychar30 char(30);
+-- create variable cmpsys.mydate date;
+-- call cmpsys.RetrieveSeniorEmployee('939',cmpsys.mychar30, cmpsys.mydate);
+-- values(cmpsys.mychar30, cmpsys.mydate);
+
